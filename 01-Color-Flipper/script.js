7fa13c12e186184a8f9dd5afd588acc3c940cc01
Flavio Mattos <flaviomdev@gmail.com>
2023-07-21 14:44:39 -0300

    Color Flipper done

---
+++ 01-Color-Flipper/script.js
@@ -0,0 +1,23 @@
+const colors = [
+	"Blue ",
+	"Green",
+	"Red",
+	"Orange",
+	"Violet",
+	"Indigo",
+	"Yellow ",
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+	const randomNumber = getRandomNumber();
+
+	document.body.style.backgroundColor = colors[randomNumber];
+	color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * colors.length);
+}
